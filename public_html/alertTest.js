775db1631a73576c2754a8984109ee4a5c25632a
Linda Khalaf <khalaflb24@gmail.com>
2014-09-03 11:20:00 -0700

    test commit

---
+++ public_html/alertTest.js
@@ -0,0 +1,63 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function call2(){
+    
+    alert("Please Click on the link 'About Me'");
+    
+}
+
+function call3(){
+    
+    alert("Please Click on the link 'APP Academy'");
+    
+}
+
+function call4(){
+    
+    alert("Please Click on the link 'Projects'");
+    
+}
+
+function call5(){
+    
+    alert("Please Click on the link 'Home' to go back to the first page");
+    
+}
+
+function parachange(){
+    document.getElementById("para1").innerHTML = Date();   
+}
+
+function who(){
+    
+    document.getElementById("who").innerHTML = "fezzes are red, the tardis is blue, bowties are cool, and so are you! -Matt Smith";
+    document.getElementById("who").style.backgroundColor = "pink";
+    document.getElementById("who").style.textcolor = "black";
+}
+
+function who2(){
+    
+    document.getElementById("who2").innerHTML = "just stand there cause i am going to hug you- David Tennant ";
+    document.getElementById("who2").style.backgroundColor = "lightgreen";
+    document.getElementById("who2").style.textcolor = "black";
+}
+
+function who3(){
+    
+    document.getElementById("who3").innerHTML = "1-2-3-4 i declare a time war; 5-6-7-8 dialeks sceam EXTERMINATE; 9-10-11-12 the doctor died and silence fell; 12-11-10-9 there he goes back in time; 8-7-6-5 saving everbodies lives; 4-3-2-1 grab her hand and whisper 'run' -random whovian";
+    document.getElementById("who3").style.backgroundColor = "lightblue";
+    document.getElementById("who3").style.textcolor = "black";
+}
+
+
+function who4(){
+    
+    document.getElementById("who4").innerHTML = "'I'm a madman with a box'- The Doctor";
+    document.getElementById("who4").style.backgroundColor = "lightgreen";
+    document.getElementById("who4").style.textcolor = "black";
+}
---
+++ public_html/changepara.js
@@ -0,0 +1,7 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+function parachange(){
+    document.getElementById("para1").innerHTML = Date();   
+}
